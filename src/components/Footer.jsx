4ed2adf92cae74f3a9648d5a550f3c12a912f837
Liv <w.phrush@gmail.com>
2019-06-09 14:59:33 -0500

    create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <h1 className="emoji-lg">
+      <span role="img" aria-label="Party Popper">
+        &#128064;
+      </span>
+    </h1>
+    <h3 className="title-3">
+      Hope to see you at the party!
+    </h3>
+    <div className="links-out title-3">
+      <a href="">
+        Meetup
+      </a>
+      &nbsp;
+      <a href="">
+        Twitter
+      </a>
+      &nbsp;
+      <a href="">
+        Apply
+      </a>
+    </div>
+  </footer>
+);
+
+export default Footer;
