0ed59595c826f7f7e7bdee1d0be96d946572dbd7
Liv <w.phrush@gmail.com>
2019-06-09 14:29:29 -0500

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="emoji-lg">
+      <span role="img" aria-label="Party Popper">
+        &#127881;
+      </span>
+    </h1>
+    <h2 className="title-2">
+      #FrontEndParty
+    </h2>
+    <p className="title-4">
+      A monthly&nbsp;
+      <a href="https://www.meetup.com/FrontEndParty/" target="_blank" className="link-delay rainbow-text">
+        meetup
+      </a>
+      &nbsp;in <strong>New Orleans</strong>
+      <br />
+      for <strong>front-end developers</strong> and <strong>designer professionals</strong>.
+    </p>
+    <p className="title-4">
+      Every <strong>third Tuesday</strong> at&nbsp;
+      <a href="https://www.acehotel.com/neworleans/" target="_blank" className="link-delay rainbow-text">
+        <strong>Ace Hotel</strong>
+      </a>
+    </p>
+  </header>
+);
+
+export default Header;
