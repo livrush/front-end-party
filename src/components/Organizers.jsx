cd9cf804d203b9fb7cb52fc7cd851b4c407924d7
Liv <w.phrush@gmail.com>
2019-06-12 21:12:10 -0500

    open external websites in new tabs

--- src/components/Organizers.jsx
+++ src/components/Organizers.jsx
@@ -3,45 +3,61 @@ import organizers from '../archive/organizers';
 
 const Organizers = () => (
   <div className="organizers">
-    {
-      organizers.map(organizer => (
-        <div className="organizer">
-          <img src={organizer.headshot} className="organizer-headshot" alt={`${organizer.name}-headshot`} />
-          <h4 className="organizer-name">{organizer.name}</h4>
-          <p className="organizer-social-media">
-            {
-              organizer.social.website ?
-              <a href={organizer.social.website} className="organizer-social-link link-delay rainbow-text">
-                <i className="fas fa-globe"></i>
-              </a> :
-              null
-            }
-            {
-              organizer.social.github ?
-              <a href={organizer.social.github} className="organizer-social-link link-delay rainbow-text">
-                <i className="fab fa-github"></i>
-              </a> :
-              null
-            }
-            {
-              organizer.social.linkedin ?
-              <a href={organizer.social.linkedin} className="organizer-social-link link-delay rainbow-text">
-                <i className="fab fa-linkedin"></i>
-              </a> :
-              null
-            }
-            {
-              organizer.social.twitter ?
-              <a href={organizer.social.twitter} className="organizer-social-link link-delay rainbow-text">
-                <i className="fab fa-twitter"></i>
-              </a> :
-              null
-            }
-          </p>
-          <span className="organizer-emoji" role="img">{organizer.emoji}</span>
-        </div>
-      ))
-    }
+    {organizers.map(organizer => (
+      <div className="organizer">
+        <img
+          src={organizer.headshot}
+          className="organizer-headshot"
+          alt={`${organizer.name}-headshot`}
+        />
+        <h4 className="organizer-name">{organizer.name}</h4>
+        <p className="organizer-social-media">
+          {organizer.social.website ? (
+            <a
+              href={organizer.social.website}
+              className="organizer-social-link link-delay rainbow-text"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fas fa-globe" />
+            </a>
+          ) : null}
+          {organizer.social.github ? (
+            <a
+              href={organizer.social.github}
+              className="organizer-social-link link-delay rainbow-text"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github" />
+            </a>
+          ) : null}
+          {organizer.social.linkedin ? (
+            <a
+              href={organizer.social.linkedin}
+              className="organizer-social-link link-delay rainbow-text"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin" />
+            </a>
+          ) : null}
+          {organizer.social.twitter ? (
+            <a
+              href={organizer.social.twitter}
+              className="organizer-social-link link-delay rainbow-text"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-twitter" />
+            </a>
+          ) : null}
+        </p>
+        <span className="organizer-emoji" role="img">
+          {organizer.emoji}
+        </span>
+      </div>
+    ))}
   </div>
 );
 
