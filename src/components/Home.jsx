049f37e14f31023d60b29804347d512aec527123
Liv <w.phrush@gmail.com>
2019-06-12 21:19:23 -0500

    LINT

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Content from './Content';
 import Presenters from './Presenters';
 import Organizers from './Organizers';
