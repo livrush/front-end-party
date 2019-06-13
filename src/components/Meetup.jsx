4998125ea023de96384e5fa8d1d108b5711979ac
Liv <w.phrush@gmail.com>
2019-06-12 20:08:22 -0500

    add divider between meetups

--- src/components/Meetup.jsx
+++ src/components/Meetup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Talk from './Talk';
 
 const Meetup = ({
@@ -9,25 +9,30 @@ const Meetup = ({
 }) => {
   const meetupTalks = talks.map(talk => <Talk {...talk} />);
   return (
-    <div className="meetup">
-      <div className="meetup-header">
-        <h2>#FrontEndParty {id}</h2>
-        <h3>
-          <span role="img" aria-label="Calendar">&#128197;</span>
-          &nbsp;
-          <a
-            className="rainbow-text link-delay"
-            href={meetup}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {date}
-          </a>
-        </h3>
-        {meetupTalks}
+    <Fragment>
+      <div className="meetup">
+        <div className="meetup-header">
+          <h1 className="meetup-number">#FrontEndParty {id}</h1>
+          <h2 className="meetup-date">
+            <span role="img" aria-label="Calendar">
+              &#128197;
+            </span>
+            &nbsp;
+            <a
+              className="rainbow-text link-delay"
+              href={meetup}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {date}
+            </a>
+          </h2>
+          {meetupTalks}
+        </div>
       </div>
-    </div>
-  )
+      <hr className="meetup-divider" />
+    </Fragment>
+  );
 };
 
 export default Meetup;
