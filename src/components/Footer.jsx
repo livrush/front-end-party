8b18e206649cc8b4a444f6449169fd88ac2cf7ce
Liv <w.phrush@gmail.com>
2019-06-12 21:19:55 -0500

    position footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,42 +9,43 @@ const Footer = () => (
     </h1>
     <h3 className="title-3">Hope to see you at the party!</h3>
     <div className="links-out title-3">
-      <a
-        href="https://www.meetup.com/FrontEndParty/events/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="footer-link link-delay rainbow-text"
-      >
-        <i className="fab fa-meetup" />
-      </a>
-      <a
-        href="https://twitter.com/frontendparty"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="footer-link link-delay rainbow-text"
-      >
-        <i className="fab fa-twitter" />
-      </a>
-      <a
-        href="/archive"
-        className="footer-link link-delay rainbow-text"
-      >
-        <i className="fas fa-book" />
-      </a>
-      <a
-        href="/code-of-conduct"
-        className="footer-link link-delay rainbow-text"
-      >
-        <i className="fas fa-gavel" />
-      </a>
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLScFo6zMLuMkzkbbLTuSCfkDI-gI--t-633NXpzg9jJ9fz4LHQ/viewform?formkey=dC1SQlBHdU5yS2xKODR0bjR5QTFENHc6MQ"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="footer-link link-delay rainbow-text"
-      >
-        <i className="fas fa-paper-plane" />
-      </a>
+      <div className="links-out-section">
+        <a
+          href="https://www.meetup.com/FrontEndParty/events/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link link-delay rainbow-text"
+        >
+          <i className="fab fa-meetup" />
+        </a>
+        <a
+          href="https://twitter.com/frontendparty"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link link-delay rainbow-text"
+        >
+          <i className="fab fa-twitter" />
+        </a>
+        <a href="/archive" className="footer-link link-delay rainbow-text">
+          <i className="fas fa-book" />
+        </a>
+      </div>
+      <div className="links-out-section">
+        <a
+          href="/code-of-conduct"
+          className="footer-link link-delay rainbow-text"
+        >
+          <i className="fas fa-gavel" />
+        </a>
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScFo6zMLuMkzkbbLTuSCfkDI-gI--t-633NXpzg9jJ9fz4LHQ/viewform?formkey=dC1SQlBHdU5yS2xKODR0bjR5QTFENHc6MQ"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link link-delay rainbow-text"
+        >
+          <i className="fas fa-paper-plane" />
+        </a>
+      </div>
     </div>
   </footer>
 );
